4bab7d6599c4d9e7526393de28162c0298e158a2
Gyxqq <108503818+Gyxqq@users.noreply.github.com>
2022-12-25 20:08:30 +0800

    Create mybind_lin.h

---
+++ mybind_lin.h
@@ -0,0 +1,49 @@
+#include <winsock.h>
+#include <iostream>
+#include <String>
+#include <string.h>
+#define LINUX
+#ifdef LINUX
+#define SOCKET int
+#endif
+class mybind
+{
+public:
+  struct sockaddr_in serv_addr;
+  int mybind(char ip[], int port, SOCKET sock)
+  {
+    memset(serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET; // ipv4
+    serv_addr.sin_addr.s_addr = inet_addr(ip);
+    serv_addr.sin_port = htons(port);
+    int g = bind(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    return g;
+  }
+  int mybind(std::string ip, int port, SOCKET sock)
+  {
+    char c[ip.size() + 1] = {};
+    c = ip;
+    mybind(c, port, sock);
+  }
+};
+
+class myconnect
+{
+public:
+  struct sockaddr_in serv_addr;
+  int myconnect(char ip[], int port, SOCKET sock)
+  {
+    memset(serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET; // ipv4
+    serv_addr.sin_addr.s_addr = inet_addr(ip);
+    serv_addr.sin_port = htons(port);
+    int g = connnect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    return g;
+  }
+  int myconnect(std::string ip, int port, SOCKET sock)
+  {
+    char c[ip.size() + 1] = {};
+    c = ip;
+    myconnect(c, port, sock);
+  }
+}
